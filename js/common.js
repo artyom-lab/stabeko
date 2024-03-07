8c1a5acd6b15abc62c481664013737a27b370748
artyom-lab <artyomgalkeane@gmail.com>
2024-03-07 14:37:53 +0300

    07032024

--- js/common.js
+++ js/common.js
@@ -1,9 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var content = document.querySelector('.wrapper');
-  content.classList.add('visible');
-});
-
 $(function () {
+
+  var content = $('.wrapper');
+  content.addClass('visible');
+  var linesBox = $('.lines-box');
+  setTimeout(function() {
+    linesBox.addClass('hidden');
+    setTimeout(function() {
+      linesBox.removeClass('hidden');
+    }, 1500);
+  });
 
   function headerSticky() {
     if ($(window).scrollTop() > 0) {
@@ -19,21 +24,45 @@ $(function () {
   headerSticky();
 
   $(".sandwich").on("click", function() {
-  $(".navbar").toggleClass("active");
-  $("body").toggleClass("menubar-in");
+    $(".navbar").toggleClass("active");
+    $("body").toggleClass("menubar-in");
   });
 
   var box = $('.lines-box');
   var scrollTimeout;
   setTimeout(function() {
-  $(window).scroll(function(){
+  $(window).on("scroll", function(){
     clearTimeout(scrollTimeout);
     box.addClass('scrolled');
     scrollTimeout = setTimeout(function() {
     box.removeClass('scrolled');
     }, 800);
   });
-  }, 1500); 
+  }, 1500);
+
+  $(window).on("scroll", function() {
+    if ($('.modal-close').length > 0) {
+
+      // Calculate opacity
+      var scrollTop = $(this).scrollTop();
+      var windowHeight = $(this).height();
+      var imageTop = $('.modal-close').offset().top;
+      if (imageTop < scrollTop + windowHeight) {
+        var opacity = 1 - (scrollTop + windowHeight - imageTop) / windowHeight;
+        $('.modal-close').css('opacity', opacity);
+      }
+
+      // Calculate translateY
+      var windowBottom = $(window).scrollTop() + $(window).height(); 
+      var sectionTop = $('.modal-close').offset().top; 
+      var sectionBottom = sectionTop + $('.modal-close').height(); 
+      if (windowBottom > sectionTop && windowBottom < sectionBottom) {
+        var translateY = (windowBottom - sectionTop) / 2; 
+        $('.to-review').css('transform', 'translateY(' + translateY + 'px)');
+      }
+
+    }
+  });
 
   AOS.init({
     duration: 1000,
@@ -41,74 +70,16 @@ $(function () {
     once: true,
   });
 
-// POPUP
-
-  $(window).scroll(function() {
-      var scrollTop = $(this).scrollTop();
-      var windowHeight = $(this).height();
-      var imageTop = $('.modal-close').offset().top;
-
-      if (imageTop < scrollTop + windowHeight) {
-          var opacity = 1 - (scrollTop + windowHeight - imageTop) / windowHeight;
-          $('.modal-close').css('opacity', opacity);
-      }
-    });
-
-  $(window).scroll(function() {
-      var windowBottom = $(window).scrollTop() + $(window).height(); 
-      var sectionTop = $('.modal-close').offset().top; 
-      var sectionBottom = sectionTop + $('.modal-close').height(); 
-      
-      if (windowBottom > sectionTop && windowBottom < sectionBottom) {
-          var translateY = (windowBottom - sectionTop) / 2; 
-          $('.to-review').css('transform', 'translateY(' + translateY + 'px)');
-      }
-  });
-
-    // $(window).scroll(function(){
-    //     // Проверяем, достигли ли мы нижнего края секции
-    //     if($(window).scrollTop() + $(window).height() == $(document).height()) {
-    //         // Если достигли, убираем класс .active у попапа
-    //         $('.popup').removeClass('active');
-    //     }
-    // });
-
-
-
-
-// // Получить доступ к iframe с классом "active"
-// var iframe = document.querySelector('iframe.active');
-
-// // Проверить, что iframe был найден
-// if (iframe) {
-//     // Получить объект содержимого iframe
-//     var iframeContent = iframe.contentWindow || iframe.contentDocument;
-
-//     // Проверить, что объект содержимого был успешно получен
-//     if (iframeContent.document) {
-//         // Теперь вы можете взаимодействовать с элементами страницы внутри iframe, используя стандартные методы JavaScript или jQuery
-//         // Например:
-//         var iframeBody = iframeContent.document.querySelector('body');
-//         if (iframeBody) {
-//             // Изменить стиль элемента внутри iframe
-//             iframeBody.style.backgroundColor = 'red';
-//         }
-//     } else {
-//         console.error('Не удалось получить доступ к содержимому iframe.');
-//     }
-// } else {
-//     console.error('iframe с классом "active" не найден.');
-// };
-
-$(".btn-popup").on("click", function() {
+  // POPUP
+  $(".btn-popup").on("click", function() {
     $(".popup").addClass("active");
     $("body").addClass("menubar-in");
-});
+  });
 
-$(".modal-close").on("click", function() {
+  $(".modal-close").on("click", function() {
     $(".popup").removeClass("active");
     $("body").removeClass("menubar-in");
-});
+  });
 
 });
 
